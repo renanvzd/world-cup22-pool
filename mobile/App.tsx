4cf8c72717af99358991a5c1bb5ef5019372b4b0
Renan Veronez Drechsler <renanvz@outlook.com>
2022-11-02 13:48:34 +0000

    Loading & SignIn

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { NativeBaseProvider } from 'native-base';
+import { Loading } from './src/components/Loading';
+
+
+import { THEME } from './src/styles/theme'
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello React Native</Text>
-        <StatusBar style="auto" />
-      </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 24,
-  }
-});
