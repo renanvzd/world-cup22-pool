b5f8877023364a54194fb88ce6bcc757c6d8d441
Renan Veronez Drechsler <renanvz@outlook.com>
2022-11-03 18:04:55 +0000

    Web - Create Pool

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,6 +4,7 @@ import logoImg from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/user-avatar-example.png'
 import iconCheckImg from '../assets/icon.svg';
 import { api } from '../lib/axios';
+import { FormEvent, useState } from 'react';
 
 interface HomeProps {
   poolCount: number;
@@ -12,6 +13,29 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      });
+
+      const { code } = response.data
+
+      await navigator.clipboard.writeText(code)
+
+      alert('Bolao criado com sucesso! O codigo foi copiado para a area de transferencia.')
+      setPoolTitle('')
+    } catch (err) {
+      console.log(err)
+      alert('Falha ao criar o bolao')
+    }
+
+  }
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
@@ -25,12 +49,14 @@ export default function Home(props: HomeProps) {
           </strong>
         </div>
 
-        <form className="mt-10 flex">
+        <form onSubmit={createPool} className="mt-10 flex">
           <input
-            className="flex-1 px-6 py-4 rounded bg-gray-800"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100"
             type="text"
             required
             placeholder="Qual o nome do bolao?"
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
           />
           <button
             className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
