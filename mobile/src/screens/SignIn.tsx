f5bc9f3d89edca914fa0dad0c04dcd39edde1047
Renan Veronez Drechsler <renanvz@outlook.com>
2022-11-04 09:55:42 +0000

    Mobile: SignIn - Button

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Text, Center } from 'native-base';
+import { Text, Center, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+
 import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
+
+      <Button
+        type="SECONDARY"
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Nao utilizamos nenhuma informacao alem {'\n'} do seu e-mail para criacao da sua conta.
+      </Text>
+
     </Center>
   )
 }
