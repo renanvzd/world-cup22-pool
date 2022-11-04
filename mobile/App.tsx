7fd230f8a34655c2ab47311a8b7f853c4d68d264
Renan Veronez Drechsler <renanvz@outlook.com>
2022-11-04 10:45:09 +0000

    Mobile: Context - useAuth() Hook

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
 import { Loading } from './src/components/Loading';
 
@@ -12,12 +13,14 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
