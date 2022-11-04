65a5ac28f1717d8176e924e3f0f484214d0a73a0
Renan Veronez Drechsler <renanvz@outlook.com>
2022-11-04 09:33:04 +0000

    Mobile: Logo.svg

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Text, Center } from 'native-base';
+import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        Sign In
-      </Text>
+      <Logo width={212} height={40} />
     </Center>
   )
 }
