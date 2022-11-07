7b31f22f28b2ab53216ecd3f1f0809cbaf3ad592
Renan Veronez Drechsler <renanvz@outlook.com>
2022-11-07 10:11:05 +0000

    Buscando dados no backend

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -52,7 +52,11 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     try {
       setIsUserLoading(true)
       const tokenResponse = await api.post('/users', { access_token });
-      console.log(response)
+      // console.log(response)
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`;
+
+      const userInfoResponse = await api.get('/me')
+      setUser(userInfoResponse.data.user);
 
     } catch (error) {
       console.log(error);
