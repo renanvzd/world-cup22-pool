6d071e7443d3dcc6c4e46233771edc703327dead
Renan Veronez Drechsler <renanvz@outlook.com>
2022-11-07 18:48:39 +0000

    Recarregamento da lista Polls

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native'
 
 import { api } from "../services/api";
 
@@ -37,9 +38,9 @@ export function Pools() {
     }
   }
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     fetchPools();
-  }, [])
+  }, []))
 
   return (
     <VStack flex={1} bgColor="gray.900">
